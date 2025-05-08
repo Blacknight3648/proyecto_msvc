25e1efef585a95c0bca28e87ded32d910368e366
Said <sai.torres@duocuc.cl>
2025-05-08 09:17:43 -0400

    Modificacion de pomp perfulandia cambio de nombre a application MsvcClientes

--- msvc-productos/src/main/java/com.msvc.productos/MsvcProductosApplication.java
+++ msvc-productos/src/main/java/com.msvc.productos/MsvcProductosApplication.java
@@ -1,13 +1,13 @@
-package com.msvc_clientes;
+package com.msvc.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class MsvcProductosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(MsvcProductosApplication.class, args);
 	}
 
 }
