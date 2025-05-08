95998c258acd3c2dbdce479e7f9c6b2c413f1e47
Solcito <sol.medina@duocuc.cl>
2025-05-08 11:55:19 -0400

    creacion de mscv de gestion de usuarios

---
+++ msvc-gestionusuarios/src/test/java/com/smedinamsvc_gestionusuarios/MsvcGestionusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smedinamsvc_gestionusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGestionusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
