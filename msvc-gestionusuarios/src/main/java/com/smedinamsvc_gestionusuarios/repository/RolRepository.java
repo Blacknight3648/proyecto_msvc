3dd316f3ff52aa356250c75bb3a6203dcf703f35
Solcito <sol.medina@duocuc.cl>
2025-05-13 08:37:17 -0400

    Se reparán errores de compatibilidad

--- msvc-gestionusuarios/src/main/java/com/smedinamsvc_gestionusuarios/repository/RolRepository.java
+++ msvc-gestionusuarios/src/main/java/com/smedinamsvc_gestionusuarios/repository/RolRepository.java
@@ -4,20 +4,10 @@ import com.smedinamsvc_gestionusuarios.model.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-import java.util.List;
 
 @Repository
 public interface RolRepository extends JpaRepository<Rol, Long> {
 
-    // Buscar un rol por su nombre (ej: "ADMIN", "USER", etc.)
-    Optional<Rol> findByNombre(String nombre);
-
-    // Verifica si un rol existe por su nombre
-    boolean existsByNombre(String nombre);
-
-    // Buscar todos los roles que contengan una cadena en el nombre (útil para filtros)
-    List<Rol> findByNombreContainingIgnoreCase(String nombre);
 }
 
 
