7b3ac673d10e34e7af3bf0bdbe11bd8bfc3eca13
Solcito <sol.medina@duocuc.cl>
2025-05-15 17:44:22 -0400

    model Entity resenia

--- mscv-resenia/src/main/java/com/smedinamsvc/resenia/model/Entity/Resenia.java
+++ mscv-resenia/src/main/java/com/smedinamsvc/resenia/model/Entity/Resenia.java
@@ -1,12 +1,32 @@
 package com.smedinamsvc.resenia.model.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table (name= "resenia")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Resenia {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name="id_resenia")
+    private Integer id;
+
+    @Column(name = "resenia", nullable = false)
+    @NotBlank(message = "El campo de reseña, no puede estar vacío")
+    private String resenia;
 
 }
