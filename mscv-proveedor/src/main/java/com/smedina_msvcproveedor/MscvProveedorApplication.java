e4968e6d4dfc8610f4463d70355a0e51275e840d
Solgrey <sol.medina@duocuc.cl>
2025-05-19 12:11:58 -0400

    .

---
+++ mscv-proveedor/src/main/java/com/smedina_msvcproveedor/MscvProveedorApplication.java
@@ -0,0 +1,13 @@
+package com.smedina_msvcproveedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscvProveedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscvProveedorApplication.class, args);
+	}
+
+}
---
+++ mscv-proveedor/src/test/java/com/smedina_msvcproveedor/MscvProveedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smedina_msvcproveedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscvProveedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
