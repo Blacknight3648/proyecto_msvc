d61a8eb4ada0efce64edd2da5d3d99f6313797f8
Solcito <sol.medina@duocuc.cl>
2025-05-22 16:56:46 -0400

    Proveedor de nuevo

---
+++ msvc.proveedor/src/test/java/com/smedina/msvc/proveedor/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smedina.msvc.proveedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
