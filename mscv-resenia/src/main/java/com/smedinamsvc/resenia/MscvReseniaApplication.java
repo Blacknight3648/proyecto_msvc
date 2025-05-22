bffa8779a7604b21226356f5be71f185aa921d27
Solcito <sol.medina@duocuc.cl>
2025-05-21 23:14:49 -0400

    Se prueba Resenia y se agrega H2 a resenia

--- mscv-resenia/src/main/java/com/smedinamsvc/resenia/MscvReseniaApplication.java
+++ mscv-resenia/src/main/java/com/smedinamsvc/resenia/MscvReseniaApplication.java
@@ -2,8 +2,10 @@ package com.smedinamsvc.resenia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.smedinamsvc.resenia.client")
 public class MscvReseniaApplication {
 
 	public static void main(String[] args) {
