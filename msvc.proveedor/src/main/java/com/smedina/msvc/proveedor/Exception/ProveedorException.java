654e8a0e7184856f01dc71b0fd04bed753b10dbd
Solcito <sol.medina@duocuc.cl>
2025-05-22 20:47:02 -0400

    ://

--- msvc.proveedor/src/main/java/com/smedina/msvc/proveedor/Exception/ProveedorException.java
+++
@@ -1,7 +0,0 @@
-package com.smedina_msvcproveedor.exception;
-
-public class ProveedorException extends RuntimeException {
-    public ProveedorException(String message) {
-        super(message);
-    }
-}
