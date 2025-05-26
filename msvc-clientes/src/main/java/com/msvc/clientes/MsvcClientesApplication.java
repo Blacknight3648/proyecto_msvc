d7ed359c5bfa0ef30c17abbf3eb137b07c9d4d3c
Solcito <sol.medina@duocuc.cl>
2025-05-25 22:10:51 -0400

    EnableFeignClients para clientes

--- msvc-clientes/src/main/java/com/msvc/clientes/MsvcClientesApplication.java
+++ msvc-clientes/src/main/java/com/msvc/clientes/MsvcClientesApplication.java
@@ -2,7 +2,8 @@ package com.msvc.clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableFeignClients
 @SpringBootApplication
 public class MsvcClientesApplication {
 
@@ -11,3 +12,4 @@ public class MsvcClientesApplication {
 	}
 
 }
+
