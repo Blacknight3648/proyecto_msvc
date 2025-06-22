c24af2e7c1ac08f262542b645e83cca450f29623
solgrey <sol.medina@duocuc.cl>
2025-06-21 21:37:14 -0400

    Cambios generales

--- mscv-resenia/src/main/java/com/smedinamsvc/resenia/repository/ReseniaRepository.java
+++ mscv-resenia/src/main/java/com/smedinamsvc/resenia/repository/ReseniaRepository.java
@@ -1,7 +1,5 @@
 package com.smedinamsvc.resenia.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
