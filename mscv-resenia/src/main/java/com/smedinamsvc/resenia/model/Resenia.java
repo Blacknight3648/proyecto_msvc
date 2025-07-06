6f912a55bf8be7a92cd10a855013202d45cd1fa0
solgrey <sol.medina@duocuc.cl>
2025-07-05 20:04:55 -0400

    Se arreglan preubas de Resenia

--- mscv-resenia/src/main/java/com/smedinamsvc/resenia/model/Resenia.java
+++ mscv-resenia/src/main/java/com/smedinamsvc/resenia/model/Resenia.java
@@ -7,8 +7,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-import java.time.LocalDate;
-
 @Entity
 @Table (name= "resenia")
 @Data
